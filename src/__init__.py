039c55290955e2077d41ca873d2285c77cf6c1ed
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-02-17 14:12:14 -0800

    Remove references to Boston/Seattle

--- src/__init__.py
+++ src/__init__.py
@@ -8,22 +8,22 @@ if __name__ == "__main__":
         Vehicle("Efficient Sedan", 5, 100, 40)
     ] * 3
 
-    miles_to_boston = 3061
+    distance_in_miles = 3061
     average_cost_per_gallon_of_gas = 2
     average_cost_per_night_at_hotel = 125
     max_people_per_room = 5
     average_cost_of_food_per_day_per_person = 7
-    ticket_cost_from_boston_to_seattle = 124
+    return_ticket_cost = 124
 
     results = simulate(
         vehicles,
-        miles_to_boston,
+        distance_in_miles,
         average_cost_per_gallon_of_gas,
         average_cost_per_night_at_hotel,
         max_people_per_room,
         range(4, 14 + 1),
         range(7, 14 + 1),
-        ticket_cost_from_boston_to_seattle,
+        return_ticket_cost,
         average_cost_of_food_per_day_per_person
     )
 
